f6a6d51c275f71b75b4990de80666eebed1b6324
MaliSarus <narikfox31@gmail.com>
2023-05-23 16:05:30 +0300

    fix

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
-import svgLoader from "vite-svg-loader";
-import checker from "vite-plugin-checker";
-import { resolve } from "path";
+// import svgLoader from "vite-svg-loader";
+// import checker from "vite-plugin-checker";
+// import { resolve } from "path";
 
 const lifecycle = process.env.npm_lifecycle_event;
 
@@ -17,23 +17,23 @@ export default defineNuxtConfig({
   build: {
     transpile: ["primevue", ...(lifecycle === "build" ? ["element-plus"] : [])],
   },
-  vite: {
-    plugins: [
-      svgLoader({
-        svgo: false,
-      }),
-      checker({
-        vueTsc: true,
-        eslint: {
-          lintCommand:
-            'eslint ./**/*.{ts,vue,d.ts} --ignore-pattern "./**/*.test.ts" --ignore-pattern "./**/*.stories.ts"',
-        },
-      }),
-    ],
-    resolve: {
-      alias: {
-        "@": resolve(__dirname),
-      },
-    },
-  },
+  // vite: {
+  //   plugins: [
+  //     svgLoader({
+  //       svgo: false,
+  //     }),
+  //     checker({
+  //       vueTsc: true,
+  //       eslint: {
+  //         lintCommand:
+  //           'eslint ./**/*.{ts,vue,d.ts} --ignore-pattern "./**/*.test.ts" --ignore-pattern "./**/*.stories.ts"',
+  //       },
+  //     }),
+  //   ],
+  //   resolve: {
+  //     alias: {
+  //       "@": resolve(__dirname),
+  //     },
+  //   },
+  // },
 });
