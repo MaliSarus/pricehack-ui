7078fe8256c1c59149200d5aa25102cb2c25f0f7
MaliSarus <narikfox31@gmail.com>
2023-05-29 15:43:43 +0300

    add: chat icons

---
+++ assets/icons/chat/index.ts
@@ -0,0 +1,5 @@
+import ChatIcon12 from './12.svg?component';
+import ChatIcon16 from './16.svg?component';
+import ChatIcon24 from './24.svg?component';
+
+export { ChatIcon12, ChatIcon16, ChatIcon24 };
